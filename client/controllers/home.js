bbe23b1c68da6f2109276ef67276470476683cc6
nikki bellock <nbellock@gmail.com>
2018-09-06 07:47:47 -0600

    'Message

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -2,10 +2,10 @@ Template.home.events({
   'submit .form'(event) {
     // Prevent default browser form submit
     event.preventDefault();
-     $('.submitCss').addClass('clicked')
-       setTimeout(function(){
+    $('.submitCss').addClass('clicked')
+    setTimeout(function () {
       $(".clicked").removeClass("clicked")
-      },1500);
+    }, 1500);
 
     var target = event.target;
     var keyword = target.keyword.value;
@@ -13,15 +13,15 @@ Template.home.events({
     var zipcode = target.zipcode.value || '08861';
 
     //grab lat and long if user enters zipcode
-    if(zipcode){
+    if (zipcode) {
       findzipcode(zipcode);
     }
 
     //runs the method to grab all the locaton results
-    Meteor.call('findLocations', Session.get("lat"), Session.get("long"), radius, keyword, function(err, res){
-      if(err){
+    Meteor.call('findLocations', Session.get("lat"), Session.get("long"), radius, keyword, function (err, res) {
+      if (err) {
         console.log(err);
-      } else{
+      } else {
         console.log('Success!');
         //sets jsonBody with the return
         Session.set("jsonBody", res.data.results);
@@ -38,46 +38,48 @@ Template.home.events({
 
 Template.home.helpers({
   thereAreLocations() {
-    if((Session.get("jsonBody")).length === 0){
+    if ((Session.get("jsonBody")).length === 0) {
       return false;
-    } else{
+    } else {
       return true;
     }
   },
   thereIsEmail() {
-   if((Session.get("chatWindow")) === true){
-    console.log("true");
-    return true;
-   } else{
-    return false;
-   }
- }
+    if ((Session.get("chatWindow")) === true) {
+      console.log("true");
+      return true;
+    } else {
+      return false;
+    }
+  }
 });
 
 //set as global helper to use in home and locations template
-Template.registerHelper( 'foundLocations', () => {
-  if((Session.get("jsonBody")).length === 0){
-      return "No locations found!"
-    } else{
-      return Session.get("jsonBody");
-    }
+Template.registerHelper('foundLocations', () => {
+  if ((Session.get("jsonBody")).length === 0) {
+    return "No locations found!"
+  } else {
+    return Session.get("jsonBody");
+  }
 });
 
 
 //if user enters zipcode instead of turning on location
-function findzipcode(zipcode){
-  var APIKey = Meteor.settings.public.APIKey;
-  var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?&address=" + zipcode + "&key="+ APIKey;
+function findzipcode(zipcode) {
+  // var APIKey = Meteor.settings.public.APIKey;
+  var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?&address=" + zipcode + "&key=" + "AIzaSyDBLqCOGd-_Caq1Lxzk6DknCu_v3MTrTSI";
 
-  $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-  var results = response.data;
-  //Stores latitude & longitude of zipcode
-  var lats=response.results[0].geometry.location.lat
-  var longs=response.results[0].geometry.location.lng
-  console.log(queryURL);
-  console.log(lats + "," + longs);
-  Session.set('lat', lats);
-  Session.set('long', longs);
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).done(function (response) {
+    var results = response.data;
+    //Stores latitude & longitude of zipcode
+    var lats = response.results[0].geometry.location.lat
+    var longs = response.results[0].geometry.location.lng
+    console.log(queryURL);
+    console.log(lats + "," + longs);
+    Session.set('lat', lats);
+    Session.set('long', longs);
   })
 };
-
--- server/locationMethods.js
+++ server/locationMethods.js
@@ -1,22 +1,22 @@
-var APIKey = Meteor.settings.public.APIKey;
+// var APIKey = Meteor.settings.public.APIKey;
 
 Meteor.methods({
-  findLocations: function(lat, long, radius, keyword){
-    var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius="+ radius + "&keyword="+ keyword + "&opennow=true&key=" + APIKey;
+  findLocations: function (lat, long, radius, keyword) {
+    var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=" + radius + "&keyword=" + keyword + "&opennow=true&key=" + "AIzaSyDBLqCOGd-_Caq1Lxzk6DknCu_v3MTrTSI";
     console.log(queryURL);
 
     var result = HTTP.get(queryURL, {});
     return result;
   },
-  getDetails: function(placeid){
-    var detailsURL = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeid + "&key=" + APIKey;
+  getDetails: function (placeid) {
+    var detailsURL = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeid + "&key=" + "AIzaSyDBLqCOGd-_Caq1Lxzk6DknCu_v3MTrTSI";
     console.log(detailsURL);
     var results = HTTP.get(detailsURL, {});
     console.log(results);
     return results;
   },
-  saveLocation: function(placeId, details){
-    Locations.insert({ 
+  saveLocation: function (placeId, details) {
+    Locations.insert({
       place_id: placeId,
       name: details.name,
       address: details.formatted_address,
@@ -26,12 +26,15 @@ Meteor.methods({
     });
     console.log('Added new location.');
   },
-  removeLocation: function(id){
-    Locations.remove({ _id: id });
+  removeLocation: function (id) {
+    Locations.remove({
+      _id: id
+    });
   },
-  viewDetails: function(id){
-    var results = Locations.find({ _id: id }).fetch();
+  viewDetails: function (id) {
+    var results = Locations.find({
+      _id: id
+    }).fetch();
     return results;
   }
 });
-
